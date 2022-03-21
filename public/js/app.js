7315b722e781d6beca215ca2aba37a2a94797fd1
Rohitwaghmare7 <rohit98waghmare@gmail.com>
2022-03-21 11:36:34 +0530

    changes json

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherform.addEventListener("submit", (e) =>{
 
     messageOne.textContent = "Loading..."
 
-    fetch("http://localhost:3000/weather?address="+location).then((response) =>{
+    fetch("/weather?address="+location).then((response) =>{
         response.json().then((data) =>{
             if(data.error){
                 messageOne.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const app = express()
+const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname,"../public/")
 const viewsPath = path.join(__dirname,"../templates/views")
 const partialPath = path.join(__dirname,"../templates/partials")
@@ -95,6 +96,6 @@ app.get("*",(req, res) => {
     })
 })
 
-app.listen(3000,() => {
-    console.log("server is up on port 3000")
+app.listen(port,() => {
+    console.log("server is up on port"+port)
 })
